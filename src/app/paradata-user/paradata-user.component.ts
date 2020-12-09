01e7021cb4e5b9b40c9058d1c12b0a13b27485d8
JustinBos01 <justinbos2001@gmail.com>
2020-12-09 17:13:42 +0100

    User paradata update
    + added single user receipt paradata
    + added single user phone paradata
    + filtering adjustments

--- src/app/paradata-user/paradata-user.component.ts
+++ src/app/paradata-user/paradata-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../config/config.service'
 
 @Component({
   selector: 'app-paradata-user',
@@ -6,10 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paradata-user.component.css']
 })
 export class ParadataUserComponent implements OnInit {
+  data;
+  paradata = [{ groupName: '',
+                userName: '',
+                date: '',
+                receipts: ''}];
 
-  constructor() { }
+  phoneData;
+  phoneParadata = [{  groupName: '',
+                      userName: '',
+                      phoneName: '',
+                      phoneType: '',
+                      phoneManufacturer: '',
+                      phoneModel: '',
+                      syncOrder: 0,
+                      receipts: 0,
+                      products: 0,
+                      photos: 0}];
+  chosenGroup = localStorage.getItem('chosenGroup')
+  chosenUser = localStorage.getItem('chosenUser')
+  constructor(
+    public configService: ConfigService
+  ) { }
 
   ngOnInit(): void {
+    this.getAmountOfReceipts()
+    this.getPhoneParadata()
   }
 
+  getAmountOfReceipts() {
+    this.paradata.length = 0;
+    this.configService.getAmountOfReceipts().subscribe(data => {
+      this.data = data;
+      this.data = this.data.receiptsPerDays
+      for (let currentParadata of this.data){
+        if (currentParadata.userName == localStorage.getItem('chosenUser')){
+          this.paradata.push(currentParadata);
+        }
+      }
+      console.log(this.paradata);
+      
+    })
+  }
+
+  getPhoneParadata() {
+    this.phoneParadata.length = 0;
+    this.configService.getPhoneParadata().subscribe(data => {
+      this.phoneData = data;
+      this.phoneData = this.phoneData.receiptsPerPhones;
+      console.log(this.phoneData)
+      for (let currentPhoneData of this.phoneData){
+        if (currentPhoneData.userName == localStorage.getItem('chosenUser')){
+          this.phoneParadata.push(currentPhoneData);
+        }
+      }
+      
+      
+    })
+  }
 }
